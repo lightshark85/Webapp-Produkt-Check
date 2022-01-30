87b37ef8190f47019e54fa4b0f005c89b3dec112
lightshark85 <bernhard25002@gmail.com>
2022-01-30 20:08:04 +0100

    test

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+
+
+
+
 const labels = [
   'January',
   'February',
@@ -15,10 +19,4 @@ const data = {
     borderColor: 'rgb(255, 99, 132)',
     data: [0, 10, 5, 2, 20, 30, 45],
   }]
-};
-
-const config = {
-  type: 'line',
-  data: data,
-  options: {}
 };
