aa01e390349dfb88bb7509dc8e136d5130b6335b
lightshark85 <bernhard25002@gmail.com>
2022-01-30 20:36:55 +0100

    names

--- main.js
+++ main.js
@@ -3,21 +3,40 @@
 
 
 const labels = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
+  'Kundennutzen',
+  'Innovativität',
+  'Technische Qualität',
+  'Zubehör, Optionen',
+  'Dokumentation',
+  'Serviceleistungen',
+  'Marktpreis',
+  'Händlerrabatte',
+  'Preis/Leistung',
+  'Flächendeckung',
+  'Qualität Vertrieb',
+  'Motivation Vertrieb',
+  'Abnehmerbindung',
+  'Lieferzeit',
+  'Liefertreue',
+  'Bekanntheitsgrad',
+  'Produktimage',
+  'Werbemaßnahmen',
+  'Verkaufsförderung',
+  'Pressearbeit',
+  'Erzielte Umsätze',
+  'Erzielte Gewinne (DB)',
+  'Beschaffungssituation',
+  'F&E Kompetenz',
+  'Produktteam',
 ];
 
 const data = {
   labels: labels,
   datasets: [{
-    label: 'My First dataset',
+    label: 'Produkt 1',
     backgroundColor: 'rgb(255, 99, 132)',
     borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
+    data: [-3, 0, 3, 1, 1.5, 2, 3],
   }]
 };
 
