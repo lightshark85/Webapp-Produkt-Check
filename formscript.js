6320cec199973eb54f324045c7b9429fb530c3e8
Phantom-448 <pruklemanuel@gmail.com>
2022-02-03 13:05:35 +0100

    User Input Test

--- formscript.js
+++ formscript.js
@@ -1,10 +1,33 @@
 
-    function displayRadioValue() {
-    var ele = document.getElementsByName('fav_language');
 
-    for(i = 0; i < ele.length; i++) {
-    if(ele[i].checked)
-    document.getElementById("result").innerHTML
-    = "Gender: "+ele[i].value;
-}
-}
+
+        function displayInput() {
+
+
+
+            document.getElementById("result-text").innerHTML = "";
+            var text = document.getElementById('Productname')
+
+            document.getElementById("result-text").innerHTML
+                += "Text String eingeben" +"<br>";
+
+
+
+            document.getElementById("result").innerHTML = "";
+            var ele = document.getElementsByClassName('rating')
+
+
+            for (i = 0; i < ele.length; i++) {
+
+                if (ele[i].type === "radio") {
+
+                    if (ele[i].checked)
+                        document.getElementById("result").innerHTML
+                            += ele[i].name + " Wert:"
+                            + ele[i].value + "<br>";
+                }
+            }
+        }
+
+
+
