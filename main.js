7c9fd59280f091f21262f65156ed2dc2e2ce37b4
lightshark85 <48909422+lightshark85@users.noreply.github.com>
2022-02-03 11:00:25 +0100

    var input data

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const data = {
     label: 'Produkt 1',
     backgroundColor: 'rgb(255, 99, 132)',
     borderColor: 'rgb(255, 99, 132)',
-    data: [-3, 0, 3, 1, 1.5, 2, 3],
+    data: [0, 0, 0, 0, 0, 0, 0],
   }]
 };
 
@@ -45,3 +45,23 @@ const config = {
   data: data,
   options: {}
 };
+
+//get input values
+function graphMe(){
+  
+ var v1 = parseInt(document.getElementById("input2").value);
+ var v2 = parseInt(document.getElementById("input3").value);
+ var v3 = parseInt(document.getElementById("input4").value);
+
+if (!v1) { v1 = 0; }
+if (!v2) { v2 = 0; }
+if (!v3) { v3 = 0; }
+
+myChart.data.datasets[0].data[0] = v1;
+myChart.data.datasets[1].data[0] = v2;
+myChart.data.datasets[2].data[0] = v3;
+myChart.data.datasets[3].data[0] = v1;
+myChart.data.datasets[4].data[0] = v2;
+myChart.data.datasets[5].data[0] = v3;
+myChart.update();  
+}
