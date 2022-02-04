9485bee3539f105755cbb98789573b6eefba3002
Phantom-448 <pruklemanuel@gmail.com>
2022-02-04 11:59:33 +0100

    User Input Test

--- formscript.js
+++ formscript.js
@@ -1,34 +1,33 @@
+function displayInput() {
+
+
+    document.getElementById("result-text").innerHTML = "";
+    var text = document.getElementById('Productname').value
+    var name = document.getElementById('Productname').name
+
+    if (text) {
+
+        document.getElementById("result-text").innerHTML
+            += name + ": " + text + "<br>";
+    }
+
+    document.getElementById("result").innerHTML = "";
+    var ele = document.getElementsByClassName('rating')
 
 
 
-        function displayInput() {
 
 
+    for (let i = 0; i < ele.length; i++) {
 
-            document.getElementById("result-text").innerHTML = "";
-            var text = document.getElementById('Productname').value
-            var name = document.getElementById('Productname').name
+        if (ele[i].type === "radio") {
 
-            document.getElementById("result-text").innerHTML
-                += name +": "+text +"<br>";
-
-
-
-            document.getElementById("result").innerHTML = "";
-            var ele = document.getElementsByClassName('rating')
-
-
-            for (i = 0; i < ele.length; i++) {
-
-                if (ele[i].type === "radio") {
-
-                    if (ele[i].checked)
-                        document.getElementById("result").innerHTML
-                            += ele[i].name + " Wert:"
-                            + ele[i].value + "<br>";
-                }
-            }
+            if (ele[i].checked)
+                document.getElementById("result").innerHTML
+                    += ele[i].name + " Wert:"
+                    + ele[i].value + "<br>";
         }
-
+    }
+}
 
 
--- main.js
+++ main.js
@@ -1,67 +1,69 @@
-
-
-
-
 const labels = [
-  'Kundennutzen',
-  'Innovativität',
-  'Technische Qualität',
-  'Zubehör, Optionen',
-  'Dokumentation',
-  'Serviceleistungen',
-  'Marktpreis',
-  'Händlerrabatte',
-  'Preis/Leistung',
-  'Flächendeckung',
-  'Qualität Vertrieb',
-  'Motivation Vertrieb',
-  'Abnehmerbindung',
-  'Lieferzeit',
-  'Liefertreue',
-  'Bekanntheitsgrad',
-  'Produktimage',
-  'Werbemaßnahmen',
-  'Verkaufsförderung',
-  'Pressearbeit',
-  'Erzielte Umsätze',
-  'Erzielte Gewinne (DB)',
-  'Beschaffungssituation',
-  'F&E Kompetenz',
-  'Produktteam',
+    'Kundennutzen',
+    'Innovativität',
+    'Technische Qualität',
+    'Zubehör, Optionen',
+    'Dokumentation',
+    'Serviceleistungen',
+    'Marktpreis',
+    'Händlerrabatte',
+    'Preis/Leistung',
+    'Flächendeckung',
+    'Qualität Vertrieb',
+    'Motivation Vertrieb',
+    'Abnehmerbindung',
+    'Lieferzeit',
+    'Liefertreue',
+    'Bekanntheitsgrad',
+    'Produktimage',
+    'Werbemaßnahmen',
+    'Verkaufsförderung',
+    'Pressearbeit',
+    'Erzielte Umsätze',
+    'Erzielte Gewinne (DB)',
+    'Beschaffungssituation',
+    'F&E Kompetenz',
+    'Produktteam',
 ];
 
 const data = {
-  labels: labels,
-  datasets: [{
-    label: 'Produkt 1',
-    backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: [0, 0, 0, 0, 0, 0, 0],
-  }]
+    labels: labels,
+    datasets: [{
+        label: 'Produkt 1',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 0, 0, 0, 0, 0, 0],
+    }]
 };
 
 const config = {
-  type: 'line',
-  data: data,
-  options: {}
+    type: 'line',
+    data: data,
+    options: {}
 };
 
 //get input values
-function graphMe(){
-  
- var v1 = parseInt(document.getElementById("input2").value);
- var v2 = parseInt(document.getElementById("input3").value);
- var v3 = parseInt(document.getElementById("input4").value);
+function graphMe() {
 
-if (!v1) { v1 = 0; }
-if (!v2) { v2 = 0; }
-if (!v3) { v3 = 0; }
+    var v1 = parseInt(document.getElementById("input2").value);
+    var v2 = parseInt(document.getElementById("input3").value);
+    var v3 = parseInt(document.getElementById("input4").value);
 
-myChart.data.datasets[0].data[0] = v1;
-myChart.data.datasets[1].data[0] = v2;
-myChart.data.datasets[2].data[0] = v3;
-myChart.data.datasets[3].data[0] = v1;
-myChart.data.datasets[4].data[0] = v2;
-myChart.data.datasets[5].data[0] = v3;
-myChart.update();  
+    if (!v1) {
+        v1 = 0;
+    }
+    if (!v2) {
+        v2 = 0;
+    }
+    if (!v3) {
+        v3 = 0;
+    }
+
+    myChart.data.datasets[0].data[0] = v1;
+    myChart.data.datasets[1].data[0] = v2;
+    myChart.data.datasets[2].data[0] = v3;
+    myChart.data.datasets[3].data[0] = v1;
+    myChart.data.datasets[4].data[0] = v2;
+    myChart.data.datasets[5].data[0] = v3;
+    myChart.update();
 }
